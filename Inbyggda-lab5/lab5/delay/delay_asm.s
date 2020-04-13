e6c6195b41bada6ae371d3126f400c13185ed20b
Abdirahman <60255179+abdiomar96@users.noreply.github.com>
2020-04-13 16:36:40 +0200

    Add files via upload

---
+++ Inbyggda-lab5/lab5/delay/delay_asm.s
@@ -0,0 +1,90 @@
+﻿/*
+ * delay_asm.s
+ *
+ * Author:	Abdirahman Omar Ali & Henrik Goth
+ *
+ * Date:	10/1-20
+ */ 
+
+ /*
+	Syftet med denna filen är att ha fördröjning mellan
+	nästa instruktion där filen kallas.
+
+ */
+
+.global delay_1_micros
+.global delay_micros
+.global delay_ms
+.global delay_s
+
+
+ ;==============================================================================
+; Delay of 1 µs (including RCALL)
+;==============================================================================
+
+delay_1_micros:
+	NOP	
+	NOP
+	RET
+
+;==============================================================================
+; Delay of X µs
+;	LDI + RCALL = 4 cycles
+;==============================================================================
+delay_micros:
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	DEC R24
+	CPI R24, 0		
+	BRNE delay_micros
+	RET
+
+;==============================================================================
+; Delay of X ms
+;	LDI + RCALL = 4 cycles
+;==============================================================================
+delay_ms:
+	MOV R18,R24
+loop_dms:
+	LDI R24, 250
+	RCALL delay_micros
+	LDI R24, 250
+	RCALL delay_micros
+	LDI R24, 250
+	RCALL delay_micros
+	LDI R24, 250
+	RCALL delay_micros
+	DEC R18
+	CPI R18, 0
+	BRNE loop_dms		
+	RET
+
+;==============================================================================
+; Delay of X s
+;	LDI + RCALL = 4 cycles
+;==============================================================================
+delay_s:
+	MOV R19, R24
+loop_s:				;delay_1_s
+	LDI R24, 250
+	RCALL delay_ms
+	LDI R24, 250
+	RCALL delay_ms
+	LDI R24, 250
+	RCALL delay_ms
+	LDI R24, 250
+	RCALL delay_ms
+	DEC R19
+	CPI R19, 0
+	BRNE loop_s
+	RET
---
+++ Inbyggda-lab5/lab5/lab5.c
@@ -0,0 +1,119 @@
+/*
+ * lab5.c
+ *
+ * Labb5.c file contains code where the main program
+ * is using a state machine following the Moores principle. Components 
+ * used for this to achieve the information is a Temperature sensor, Arduino Leonardo and
+ * Hitachi LCD display 
+ *
+ * Created: 2020-01-13 17:33:22
+ * Author : Henrik Goth & Abdirahman Omar Ali
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+#include "hmi/hmi.h"
+#include "numkey/numkey.h"
+#include "temp/temp.h"
+#include "lcd/lcd.h"
+#include "delay/delay.h"
+
+char temp_str[17];
+char *p_str1 = "Temperature:  ";
+
+enum state{
+
+	SHOW_TEMP_C,
+	SHOW_TEMP_F,
+	SHOW_TEMP_CF
+	
+};
+
+typedef enum state state_t;
+
+
+
+int main(void)
+{	
+	// initialize HMI (LCD and numeric keyboard)
+	hmi_init();
+	
+	//Initialize the ADC and ISR.
+	temp_init();
+	
+	
+	
+
+	uint8_t key;
+	key =  numkey_read();
+	
+	enum state CURRENT_STATE = SHOW_TEMP_C;
+	enum state NEXT_STATE;
+	
+	//sprintf(temp_str, "%u%cC", temp_read_celsius(), 0xDF);
+	//output_msg(p_str1, temp_str, 0);
+	
+
+
+
+
+	
+    /* State Machine for the different modes when pressing keys 1,2, or 3 */
+    while (1) 
+    {
+		switch(CURRENT_STATE){
+			//First state
+			case SHOW_TEMP_C:
+			sprintf(temp_str, "%u%cC", temp_read_celsius(), 0xDF);
+			
+			output_msg(p_str1, temp_str, 0);
+			if (numkey_read()== NO_KEY || numkey_read() == '1'){
+				NEXT_STATE = SHOW_TEMP_C;
+				
+			} if (numkey_read()== '2'){
+				NEXT_STATE = SHOW_TEMP_F;
+				
+			}  if (numkey_read() == '3'){
+				NEXT_STATE = SHOW_TEMP_CF;
+			}
+			CURRENT_STATE = NEXT_STATE;
+			break;
+			{
+				case SHOW_TEMP_F:
+				sprintf(temp_str, "%u%cF", temp_read_fahrenheit(), 0xDF);
+				output_msg(p_str1, temp_str, 0);
+			if (numkey_read()== NO_KEY || numkey_read() == '2'){
+				NEXT_STATE = SHOW_TEMP_F;
+				
+				} if (numkey_read()== '1'){
+				NEXT_STATE = SHOW_TEMP_C;
+				
+				} if (numkey_read() == '3'){
+				NEXT_STATE = SHOW_TEMP_CF;
+			}
+			CURRENT_STATE = NEXT_STATE;
+			break;	
+			}
+			
+			case SHOW_TEMP_CF:
+			sprintf(temp_str,"%u%cC " "%u%cF", temp_read_celsius(),
+			 0xDF, temp_read_fahrenheit(), 0xDF);
+			output_msg(p_str1, temp_str, 0);
+			{
+				if (numkey_read()== NO_KEY || numkey_read() == '3'){
+					NEXT_STATE = SHOW_TEMP_CF;
+					
+					} if (numkey_read()== '2'){
+					NEXT_STATE = SHOW_TEMP_F;
+					
+					} if (numkey_read() == '1'){
+					NEXT_STATE = SHOW_TEMP_C;
+				}
+				CURRENT_STATE = NEXT_STATE;
+				break;
+			}
+		}
+		
+    }
+}
+
